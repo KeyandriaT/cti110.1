480e43cf619e3397f152119337e10f85a03d089d
KeyandriaT <114114259+KeyandriaT@users.noreply.github.com>
2022-09-26 13:21:13 -0400

    Add files via upload
    Description: Travel expenses for a trip to Miami
    Date:9-26-2022
    CTI-110 P1HW2-Travel Expense
    Keyandria Thompson

---
+++ P1HW2_ThompsonKeyandria.py
@@ -0,0 +1,23 @@
+Python 3.10.7 (tags/v3.10.7:6cc6b13, Sep  5 2022, 14:08:36) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> budget=5000
+... destination=('Miami')
+... gas=300
+... accomodation=800
+... food=200
+... expenses=gas+accomodation+food
+... results=budget-expenses
+... 
+... print('What is your budget?:')
+... print(budget)
+... print('What is the travel destination?:')
+... print(destination)
+... print('How much will be spent on gas?:')
+... print(gas)
+... print('How much will be used for accomodation?:')
+... print(accomodation)
+... print('How much will be spent on food?:')
+... print(food)
+... print('Please add expenses:')
+... print(expenses)
+... print('Please subtract expenses from budget for remaining balance:')
